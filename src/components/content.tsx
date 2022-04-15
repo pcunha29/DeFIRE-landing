1f67fcffa138ac210c42e076a845447407b6e6ff
pcunhadev <pcunhadev@gmail.com>
2022-04-15 17:55:47 +0100

    code improvment

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,7 +1,4 @@
-import blazeIcon from "../images/blazeIcon.svg";
-import gDEFIREIcon from "../images/gDEFIREIcon.svg";
-import igniteIcon from "../images/igniteIcon.svg";
-import DAO from "../images/DAO.svg";
+import { weOfferSection, tokensSection, DAOSection } from "../utils/texts";
 
 function Content() {
   return (
@@ -34,67 +31,23 @@ function Content() {
           </p>
         </div>
       </section>
-      {/* 3rd Text Section */}
+
+      {/* WHAT WE OFFER Section */}
       <section className="container px-4">
         <div className="grid grid-cols-2 gap-4 text-defire-water">
-          <div className="flex mb-40">
-            <p className="w-20">[ 01 ]</p>
-            <div className="w-1/2 font-normal text-2xl uppercase">
-              <p className="mb-4">
-                Build a Portfolio of Containerised Strategies
-              </p>
-              <p className="text-base pt-4 normal-case">
-                The complexity of yield opportunities In DeFi is rising, Many
-                investors do not have the time, effort, or technical knowledge
-                to monitor, conceive, and implement these strategies themselves.
-                DeFIRE offers exposure to the market through a diversified
-                marketplace of containers with varying risk, yield and security
-                levels.
-              </p>
-            </div>
-          </div>
-          <div className="flex mb-40">
-            <p className="w-20">[ 02 ]</p>
-            <div className="w-1/2 font-normal text-2xl uppercase">
-              <p className="mb-4"> The Most Efficient Way to Invest </p>
-              <p className="text-base pt-4 normal-case">
-                Shape your future and build a portfolio with composable
-                allocation strategies. Maximise your returns while remaining
-                inside your risk comfort zone.
-              </p>
-            </div>
-          </div>
-          <div className="flex mb-40">
-            <p className="w-20">[ 03 ]</p>
-            <p className="w-1/2 font-normal text-2xl uppercase">
-              <p className="mb-4">
-                Market Leaders Compete to Optimise Your Capital
-              </p>
-              <p className="text-base pt-4 normal-case">
-                A marketplace as your single point of contact to the world of
-                DeFi. From the most daring strategies to the ones that fit your
-                risk appetite and capital growth profile, let leading protocols
-                and professional fund managers compete to offer you the most
-                competitive yield for your investment.
-              </p>
-            </p>
-          </div>
-          <div className="flex mb-40">
-            <p className="w-20">[ 04 ]</p>
-            <div className="w-1/2 font-normal text-2xl uppercase">
-              <p className="mb-4 flex flex-col">
-                <span>Fixed Supply;</span> <span>Expanding Utility </span>
-              </p>
-              <p className="text-base pt-4 normal-case">
-                The two key factors in value accrual and positive price action.
-                Governed by a multipurpose $DFIRE crypto token that aggregates a
-                reward share from all the ecosystem protocols. Truly low touch,
-                passive income.
-              </p>
-            </div>
-          </div>
+          {weOfferSection &&
+            weOfferSection.map((data, i) => (
+              <div key={i} className="flex mb-40">
+                <p className="w-20">{data.number}</p>
+                <div className="w-1/2 font-normal text-2xl uppercase">
+                  <p className="mb-4">{data.title}</p>
+                  <p className="text-base pt-4 normal-case">{data.body}</p>
+                </div>
+              </div>
+            ))}
         </div>
       </section>
+
       {/* 4th Text Section */}
       <section className="container h-80 mt-52">
         <div className="relative flex text-defire-water">
@@ -108,80 +61,39 @@ function Content() {
           </p>
         </div>
       </section>
-      {/* 5th Text Section */}
+
+      {/* TOKEN Section */}
       <section className="container">
         <div className="grid grid-cols-3">
-          <div className="text-defire-water w-80">
-            <div className="flex">
-              <img src={blazeIcon} alt="" />
-              <p className="pt-4 pl-20 text-2xl">DEFIRE BLAZE</p>
-            </div>
-            <p className="text-base mt-7">
-              BLAZE is a marketplace where you can deposit your assets in
-              exchange for full-on strategies utilising various assets and
-              protocols. Strategy containers offer both precision in the
-              selected strategies and extensive exposure to a diversity of
-              market capital. Create a personalised portfolio to achieve your
-              investment goals by selecting containers matching your risk
-              appetite.
-            </p>
-          </div>
-
-          <div className="text-defire-water w-80">
-            <div className="flex">
-              <img src={gDEFIREIcon} alt="" />
-              <p className="pt-4 pl-20 text-2xl">gDEFIRE</p>
-            </div>
-            <p className="text-base mt-7">
-              DeFIRE is an ecosystem of synergistic protocols governed by a
-              single token. Locking $DFIRE into $gDFIRE provides investors with
-              governance power over DeFIRE’s ecosystem. gDFIRE holders receive a
-              share of the revenue generated across all current and future
-              protocols.
-            </p>
-          </div>
-
-          <div className="text-defire-water w-80">
-            <div className="flex">
-              <img src={igniteIcon} alt="" />
-              <p className="pt-4 pl-20 text-2xl">DEFIRE IGNITE</p>
-            </div>
-            <p className="text-base mt-7">
-              IGNITE is a lending platform which enables investors in BLAZE
-              containers to use their container shares as collateral. Unlock
-              your investments by making them liquid without selling them.
-              IGNITE will automatically repay your loan using the yield
-              generated from your collateral.
-            </p>
-          </div>
+          {tokensSection &&
+            tokensSection.map((data, i) => (
+              <div key={i} className="text-defire-water w-80">
+                <div className="flex">
+                  <img src={data.icon} alt="" />
+                  <p className="pt-4 pl-20 text-2xl">{data.title}</p>
+                </div>
+                <p className="text-base mt-7">{data.body}</p>
+              </div>
+            ))}
         </div>
       </section>
-      {/* 6th Section */}
+
+      {/* DAO Section */}
       <section className="container mt-52">
         <div className="relative flex flex-col text-defire-water">
           <p className="flex flex-col text-3xl text-left font-medium uppercase mb-10">
             <p>
-              <span className="text-sm pr-12">[ DeFIRE is governed as a ]</span>
-              modular and highly scalable
+              <span className="text-sm pr-12">{DAOSection.title_small}</span>
+              {DAOSection.title_normal}
             </p>
-            <p> Decentralized Autonomous Organization</p>
+            <p>{DAOSection.title_normal_2}</p>
           </p>
           <div className="grid grid-cols-2">
-            <img src={DAO} alt="" />
+            <img src={DAOSection.image} alt="" />
             <p className="text-base ml-28 w-80 flex flex-col self-end">
-              <span className="text-2xl pb-2">DAO</span>
-              <span className="pb-5">
-                The new generation of decentralised autonomous organisation
-                features contributors working in self-organised groups. Fair
-                compensation attracts top talent and retains it.
-              </span>
-              <span>
-                Transparently and fairly governed by $gDFIRE holders, DeFIRE DAO
-                empowers individuals to realise their potential by creating new
-                opportunities, borderless communities, and inclusive financial
-                products. DeFIRE is changing the relationship between capital
-                providers, protocols, and their end users.
-              </span>
+              <span className="text-2xl pb-2">{DAOSection.subtitle}</span>
+              <span className="pb-5">{DAOSection.content}</span>
+              <span>{DAOSection.content_2}</span>
             </p>
           </div>
         </div>
---
+++ src/utils/texts.jsx
@@ -0,0 +1,57 @@
+import blazeIcon from "../images/blazeIcon.svg";
+import gDEFIREIcon from "../images/gDEFIREIcon.svg";
+import igniteIcon from "../images/igniteIcon.svg";
+import DAO from "../images/DAO.svg";
+
+export const weOfferSection = [
+  {
+    number: "[ 01 ]",
+    title: "Build a Portfolio of Containerised Strategies",
+    body: "The complexity of yield opportunities In DeFi is rising, Many investors do not have the time, effort, or technical knowledge to monitor, conceive, and implement these strategies themselves. DeFIRE offers exposure to the market through a diversified marketplace of containers with varying risk, yield and security levels.",
+  },
+  {
+    number: "[ 02 ]",
+    title: "The Most Efficient Way to Invest",
+    body: "Shape your future and build a portfolio with composable allocation strategies. Maximise your returns while remaining inside your risk comfort zone.",
+  },
+  {
+    number: "[ 03 ]",
+    title: "Market Leaders Compete to Optimise Your Capital",
+    body: "A marketplace as your single point of contact to the world of DeFi. From the most daring strategies to the ones that fit your risk appetite and capital growth profile, let leading protocols and professional fund managers compete to offer you the most competitive yield for your investment.",
+  },
+  {
+    number: "[ 04 ]",
+    title: "Fixed Supply; Expanding Utility",
+    body: "The two key factors in value accrual and positive price action. Governed by a multipurpose $DFIRE crypto token that aggregates a reward share from all the ecosystem protocols. Truly low touch, passive income.",
+  },
+];
+
+export const tokensSection = [
+  {
+    icon: blazeIcon,
+    title: "DEFIRE BLAZE",
+    body: "BLAZE is a marketplace where you can deposit your assets in exchange for full-on strategies utilising various assets and protocols. Strategy containers offer both precision in the selected strategies and extensive exposure to a diversity of market capital. Create a personalised portfolio to achieve your investment goals by selecting containers matching your risk appetite.",
+  },
+  {
+    icon: gDEFIREIcon,
+    title: "gDEFIRE",
+    body: " DeFIRE is an ecosystem of synergistic protocols governed by a single token. Locking $DFIRE into $gDFIRE provides investors with governance power over DeFIRE’s ecosystem. gDFIRE holders receive a share of the revenue generated across all current and future protocols.",
+  },
+  {
+    icon: igniteIcon,
+    title: "DEFIRE IGNITE",
+    body: " IGNITE is a lending platform which enables investors in BLAZE containers to use their container shares as collateral. Unlock your investments by making them liquid without selling them. IGNITE will automatically repay your loan using the yield generated from your collateral.",
+  },
+];
+
+export const DAOSection = {
+  title_small: "[ DeFIRE is governed as a ]",
+  title_normal: "modular and highly scalable",
+  title_normal_2: "Decentralized Autonomous Organization",
+  subtitle: "DAO",
+  image: DAO,
+  content:
+    " The new generation of decentralised autonomous organisation features contributors working in self-organised groups. Fair compensation attracts top talent and retains it.",
+  content_2:
+    "Transparently and fairly governed by $gDFIRE holders, DeFIRE DAO empowers individuals to realise their potential by creating new opportunities, borderless communities, and inclusive financial products. DeFIRE is changing the relationship between capital providers, protocols, and their end users.",
+};
